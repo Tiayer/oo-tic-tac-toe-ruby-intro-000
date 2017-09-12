890d03600df86df1cd6cdd15367d65c582cc457e
Leonard Chew <leochew@gmail.com>
2017-09-12 12:25:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,4 +67,17 @@ def current_player
   end
 end
 
+def turn
+puts "Please enter 1-9:"
+user_input = gets.strip
+index = input_to_index(user_input)
+
+if valid_move?(index) == true
+  move(index, current_player)
+  display_board
+else
+  turn
+end
+end
+
 end
